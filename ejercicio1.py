6f0450d7f0b118c3d9cdb071418346523772cec9
David Ruiz <113980722+davidruiiz@users.noreply.github.com>
2022-12-09 11:29:34 +0100

    solución

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,24 +1,24 @@
+# Definimos una función que tome una cadena de caracteres y devuelva un diccionario con las subcadenas y su puntuación
 def minion_game(string):
-    # your code goes here
-    vowels = 'AEIOU'
-    kevin_score = 0
-    stuart_score = 0
-    for i in range(len(string)):
-        if string[0] in vowels:
-            kevin_score += (len(string)-i)
-        else:
-            stuart_score += (len(string)-i)
-    if kevin_score > stuart_score:
-        print ("Kevin", kevin_score)
-    elif kevin_score < stuart_score:
-        print ("Stuart", stuart_score)
+  # Creamos un diccionario vacío que almacenará las subcadenas y su puntuación
+  substrings = {}
+  
+  # Iteramos sobre todas las letras de la cadena
+  for i in range(len(string)):
+    # Stuart forma palabras que comiencen con consonantes
+    if string[i] not in "AEIOU":
+      # Iteramos sobre todas las subcadenas que comiencen con la letra actual
+      for j in range(i + 1, len(string) + 1):
+        # Agregamos la subcadena al diccionario y aumentamos su puntuación en 1
+        substrings[string[i:j]] = substrings.get(string[i:j], 0) + 1
+        
+    # Kevin forma palabras que comiencen con vocales
     else:
-        print ("Draw")
-'''
-if __name__ == '__main__':
+      # Iteramos sobre todas las subcadenas que comiencen con la letra actual
+      for j in range(i + 1, len(string) + 1):
+        # Agregamos la subcadena al diccionario y aumentamos su puntuación en 1
+        substrings[string[i:j]] = substrings.get(string[i:j], 0) + 1
+  return substrings
+# Probamos la función con la cadena "BANANA"
 
-    s = raw_input()
-    minion_game(s)
-    '''
-
-minion_game('banana')
+print(minion_game("BANANA"))
